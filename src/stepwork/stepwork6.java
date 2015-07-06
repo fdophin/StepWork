1c7a93f72e2d2f1c4b890b7403dd80db622720ee
fdophin <fort.dophin@gmail.com>
2015-07-06 18:45:51 +0300

    Create stepwork6.java

---
+++ src/stepwork/stepwork6.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package stepwork;
+
+/**
+ *
+ * @author Rand_oj17
+ */
+public class stepwork6 {
+    public static void main(String args[]) {
+       
+    //Проверяем загорел не загорел...
+    int h=0;
+    boolean sunburn = false; //первичное состояние
+    System.out.println("Я иду на море");
+    while (sunburn != true){
+    System.out.println("Загорелш!");
+    h++; // +1 ko vremeny
+    System.out.println(h);
+    if (h>=3) sunburn = true;
+    }
+    }
+    
+}
