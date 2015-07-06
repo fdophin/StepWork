ce4d23804d0f665c867893265371df00cdb85e51
fdophin <fort.dophin@gmail.com>
2015-07-06 20:07:37 +0300

    Create stepwork9.java

---
+++ src/stepwork/stepwork9.java
@@ -0,0 +1,14 @@
+package stepwork;
+
+
+public class stepwork9 {
+    public static void main(String args[]) {
+        for (int i = 0; i <= 10; i++){
+            System.out.print(i + ", ");
+        }
+      
+        
+        
+    }
+    
+}
