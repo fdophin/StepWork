c04c6d6319d6ffe26a6627d598e63e3ad6cd548f
fdophin <fort.dophin@gmail.com>
2015-07-06 20:26:47 +0300

    Create stepwork10.java

---
+++ src/stepwork/stepwork10.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package stepwork;
+
+/**
+ *
+ * @author Rand_oj17
+ */
+public class stepwork10 {
+    public static void main(String args[]) {
+        for (double c = 0, f = 32; c <= 100; System.out.println(c + " - " + f), c++, f+=1.8); 
+    // без тела цикла
+    }
+    
+}
