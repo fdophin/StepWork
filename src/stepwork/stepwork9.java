500fda119f9304d2c56ab264515fb605f952fed2
fdophin <fort.dophin@gmail.com>
2015-07-06 20:14:12 +0300

    Update stepwork9.java

--- src/stepwork/stepwork9.java
+++ src/stepwork/stepwork9.java
@@ -3,7 +3,7 @@ package stepwork;
 
 public class stepwork9 {
     public static void main(String args[]) {
-        for (int i = 0; i <= 10; i++){
+        for (int i = 0; i <= 10; i++){  //вынос за скобки (;i <= 10;)
             System.out.print(i + ", ");
         }
       
