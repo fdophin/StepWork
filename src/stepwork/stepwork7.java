992c10939072d5f96ede406429c88b0c15b8259a
fdophin <fort.dophin@gmail.com>
2015-07-06 19:13:26 +0300

    Create stepwork7.java

---
+++ src/stepwork/stepwork7.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package stepwork;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Rand_oj17
+ */
+public class stepwork7 {
+    public static void main(String args[]) {
+       //нарисовать 15 решеток
+        
+        int lenght = 15;
+        char symbol = '#';
+        int current = 0;
+        while (current < lenght){  //Либо вместо current: lenght > 0
+    System.out.print(symbol);
+    current++;                     // lenght--
+    
+    }
+    System.out.println();    
+    }
+    
+}
