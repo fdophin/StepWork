83c6f2a5784b4ab3129efbc0bea8e25a2cdf8131
fdophin <fort.dophin@gmail.com>
2015-07-06 19:52:17 +0300

    Create stepwork8.java

---
+++ src/stepwork/stepwork8.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package stepwork;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Rand_oj17
+ */
+public class stepwork8 {
+    public static void main(String args[]) {
+       
+    int len =15;
+    do { 
+    System.out.print("#");
+    len--;
+    } while (len!=0);
+    System.out.println();
+}
+}
