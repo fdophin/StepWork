b50a748f4d594f5f7cb9bf135c2e5d27d0371327
fdophin <fort.dophin@gmail.com>
2015-07-09 19:36:43 +0300

    Create stepwork12.java

---
+++ src/stepwork/stepwork12.java
@@ -0,0 +1,9 @@
+int len =10;
+       int height = 5;
+       for(int j = 0; j < len; j++){
+       for(int i = 0; i < len; i++){
+           
+           System.out.print ("*");
+        }
+    System.out.println();
+       }
